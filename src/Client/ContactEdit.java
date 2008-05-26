f810b64d6d9e4b8d86bb8b96e31ae58cf4e116a0
Daniel Apatin <daniel.apatin@gmail.com>
2008-05-26 06:24:54 +0000

    * Добавление и редактирование контакта TODO: прокачать форму до динамической, починить выбор транспорта.

--- src/Client/ContactEdit.java
+++ src/Client/ContactEdit.java
@@ -1,8 +1,9 @@
 /*
  * ContactEdit.java
  *
- * Created on 7.05.2005, 2:15
- * Copyright (c) 2005-2008, Eugene Stahov (evgs), http://bombus-im.org
+ * Created on 26.05.2008, 10:04
+ *
+ * Copyright (c) 2006-2008, Daniel Apatin (ad), http://apatin.net.ru
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
@@ -22,7 +23,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
  */
 
 package Client;
@@ -33,35 +33,36 @@ import javax.microedition.lcdui.*;
 import java.util.*;
 import locale.SR;
 import ui.controls.TextFieldEx;
+import ui.controls.form.BoldString;
+import ui.controls.form.CheckBox;
+import ui.controls.form.DefForm;
+import ui.controls.form.DropChoiceBox;
+import ui.controls.form.TextInput;
 
 /**
  *
  * @author Evg_S
  */
 public final class ContactEdit
-        implements CommandListener, ItemStateListener 
-//#if (!MIDP1)
-        , ItemCommandListener
-//#endif
-{
+        extends DefForm {
     private Display display;
     public Displayable parentView;
     
-    Form f;
-    TextField tJid;
-    TextField tNick;
-    TextField tGroup;
-    ChoiceGroup tGrpList;
-    ChoiceGroup tTranspList;
-    ChoiceGroup tAskSubscrCheckBox;
+    //Form f;
+     private TextInput tJid;
+     private TextInput tNick;
+     private TextInput tGroup;
+     //private ChoiceGroup tGrpList;
+     private DropChoiceBox tTranspList;
+     private CheckBox tAskSubscrCheckBox;
     
     int ngroups;
     
     int grpFIndex;
     
-    Command cmdOk=new Command(SR.MS_ADD, Command.OK, 1);
-    Command cmdSet=new Command(SR.MS_SET, Command.ITEM, 2);
-    Command cmdCancel=new Command(SR.MS_CANCEL,Command.BACK,99);
+    //Command cmdOk=new Command(SR.MS_ADD, Command.OK, 1);
+    //Command cmdSet=new Command(SR.MS_SET, Command.ITEM, 2);
+    //Command cmdCancel=new Command(SR.MS_CANCEL,Command.BACK,99);
     
     boolean newContact=true;
     Config cf;
@@ -70,44 +71,36 @@ public final class ContactEdit
     //StoreContact sC;
     
     public ContactEdit(Display display, Contact c) {
+        super(display, SR.MS_ADD_CONTACT);
         this.display=display;
         parentView=display.getCurrent();
         cf=Config.getInstance();
+        
         Vector groups=sd.roster.groups.getRosterGroupNames();
+
         
-        f=new Form(SR.MS_ADD_CONTACT);
+        tJid=new TextInput(display, null, null); 
+
+        tNick=new TextInput(display, null, null);
         
-        tJid=new TextFieldEx(SR.MS_USER_JID, null, 150, TextField.ANY); 
-        
-        tNick=new TextFieldEx(SR.MS_NAME, null, 32, TextField.ANY); 
-        tGroup=new TextFieldEx(SR.MS_GROUP ,null, 32, TextField.ANY);
-        
-        
-        tGrpList=new ChoiceGroup(SR.MS_GROUP , ChoiceGroup.POPUP);
-        tTranspList=new ChoiceGroup(SR.MS_TRANSPORT, ChoiceGroup.POPUP);
-        
-        tAskSubscrCheckBox=new ChoiceGroup(SR.MS_SUBSCRIPTION, ChoiceGroup.MULTIPLE);
-        tAskSubscrCheckBox.append(SR.MS_ASK_SUBSCRIPTION, null);
-        
-        
-//#if (!MIDP1)
-        //NOKIA FIX
-        tGrpList.addCommand(cmdSet);
-        tGrpList.setItemCommandListener(this);
-        
-        tTranspList.addCommand(cmdSet);
-        tTranspList.setItemCommandListener(this);
-//#endif
-        
+        tGroup=new TextInput(display, (c==null)?"":c.getGroup().name, null);
+
+        tTranspList=new DropChoiceBox(display);
         // Transport droplist
-        tTranspList.append(sd.account.getServer(), null);
+        tTranspList.append(sd.account.getServer());
         for (Enumeration e=sd.roster.getHContacts().elements(); e.hasMoreElements(); ){
             Contact ct=(Contact)e.nextElement();
             Jid transpJid=ct.jid;
             if (transpJid.isTransport()) 
-                tTranspList.append(transpJid.getBareJid(),null);
+                tTranspList.append(transpJid.getBareJid());
         }
-        tTranspList.append(SR.MS_OTHER,null);
+        tTranspList.append(SR.MS_OTHER);
+
+        
+        tAskSubscrCheckBox=new CheckBox(SR.MS_ASK_SUBSCRIPTION, false);
+        
+        //tGrpList=new ChoiceGroup(SR.MS_GROUP , ChoiceGroup.POPUP);
+
         
         try {
             String jid;
@@ -121,8 +114,8 @@ public final class ContactEdit
             }
 //#endif
             // edit contact
-            tJid.setString(jid);
-            tNick.setString(c.nick);
+            tJid.setValue(jid);
+            tNick.setValue(c.nick);
 //#ifndef WMUC
             if (c instanceof MucContact) {
                 c=null;
@@ -131,160 +124,44 @@ public final class ContactEdit
 //#endif
             if (c.getGroupType()!=Groups.TYPE_NOT_IN_LIST  && c.getGroupType()!=Groups.TYPE_SEARCH_RESULT) {
                 // edit contact
-                f.setTitle(jid);
-                cmdOk=new Command(SR.MS_UPDATE, Command.OK, 1);
+                getMainBarItem().setElementAt(jid, 0);
                 newContact=false;
             } else c=null; // adding not-in-list
-        } catch (Exception e) {c=null;} // if MucContact does not contains realJid
-        
-        
-        int sel=-1;
-        ngroups=0;
-        String grpName="";
-        if (c!=null) grpName=c.getGroup().name;
-        
-        if (groups!=null) {
-            ngroups=groups.size();
-            for (int i=0;i<ngroups; i++) {
-                String gn=(String)groups.elementAt(i);
-                tGrpList.append(gn, null);
-                
-                if (gn.equals(grpName)) sel=i;
-            }
-        }
-            
-        //if (sel==-1) sel=groups.size()-1;
-        if (sel<0) sel=0;
-        //tGroup.setString(group(sel));
-        
+        } catch (Exception e) {
+            c=null;
+        } // if MucContact does not contains realJid
         
         if (c==null){
-            f.append(tJid);
-            f.append(tTranspList);
+            itemsList.addElement(new BoldString(SR.MS_USER_JID));
+            itemsList.addElement(tJid);
+            
+            itemsList.addElement(new BoldString(SR.MS_TRANSPORT));
+            itemsList.addElement(tTranspList);
         }
-        updateChoice(tJid.getString(),tTranspList);
-        f.append(tNick);
+        itemsList.addElement(new BoldString(SR.MS_NAME));
+        itemsList.addElement(tNick);
         
-        
-        //f.append(tGroup);
-        
-        tGrpList.append(SR.MS_NEWGROUP,null);
-        tGrpList.setSelectedIndex(sel, true);
-        
-        grpFIndex=f.append(tGrpList);
-        
+        itemsList.addElement(new BoldString(SR.MS_GROUP));
+        itemsList.addElement(tGroup);
+
         if (newContact) {
-            f.append(tAskSubscrCheckBox);
-            tAskSubscrCheckBox.setSelectedIndex(0, true);
+            itemsList.addElement(new BoldString(SR.MS_SUBSCRIPTION));
+            itemsList.addElement(tAskSubscrCheckBox);
         }
         
-        f.addCommand(cmdOk);
-        f.addCommand(cmdCancel);
-        
-        f.setCommandListener(this);
-        f.setItemStateListener(this);
-        
-        display.setCurrent(f);
+        moveCursorTo(getNextSelectableRef(-1));
+        attachDisplay(display);
     }
 
-    public void commandAction(Command c, Displayable d) {
-        if (c==cmdOk) {
-            String jid=getString(tJid);
-            if (jid!=null) {
-                String name=getString(tNick);
-                String group=group(tGrpList.getSelectedIndex());
-                if (group==null) group=getString(tGroup);
-                
-                try {
-                    int gSel=tGrpList.getSelectedIndex();
-                    if (gSel!=tGrpList.size()-1)  {
-                        group=(gSel>0)? tGrpList.getString(gSel) : null; // nokia fix
-                    }
-                } catch (Exception e) {} // nokia fix
-
-                boolean ask[]=new boolean[1];
-                tAskSubscrCheckBox.getSelectedFlags(ask);
-                sd.roster.storeContact(jid, name, group, ask[0]);
-                destroyView();
-                return;
-            }
-        }
-        
-        if (c==cmdCancel) destroyView();
-    }
-    
-//#if (!MIDP1)
-    public void commandAction(Command command, Item item) {
-        itemStateChanged(item);
-    }
-//#endif
-
-    private String getString(TextField t){
-        if (t.size()==0) return null;
-        String s=t.getString().trim();
-        if (s.length()==0) return null;
-        return s;
-    }
-    
-    private String group(int index) {
-        if (index==0) return null;
-        if (index==tGrpList.size()-1) return null;
-        return tGrpList.getString(index);
-    }
-    
-    private void updateChoice(String str, ChoiceGroup grp) {
-        int sz=grp.size();
-        int set=sz-1;
-        for (int i=0; i<sz; i++) {
-            if (str.equals(grp.getString(i))) {
-                set=i;
-                break;
-            }
-        }
-        if (grp.getSelectedIndex()!=set) 
-            grp.setSelectedIndex(set, true);
-    }
-    
-    public void itemStateChanged(Item item){
-        if (item==tGrpList) {
-            int index=tGrpList.getSelectedIndex();
-            if (index==tGrpList.size()-1) {
-                f.set(grpFIndex, tGroup);
-            }
-            //tGroup.setString(group(index));
-        }
-        
-        //if (item==tGroup) {
-        //    updateChoise(tGroup.getString(), tGrpList);
-        //}
-        
-        if (item==tTranspList) {
-            int index=tTranspList.getSelectedIndex();
-            if (index==tTranspList.size()-1) return;
-            
-            String transport=tTranspList.getString(index);
-            
-            String jid=tJid.getString();
-            StringBuffer jidBuf=new StringBuffer(jid);
-            
-            int at=jid.indexOf('@');
-            if (at<0) at=tJid.size();
-            
-            jidBuf.setLength(at);
-            jidBuf.append('@');
-            jidBuf.append(transport);
-            tJid.setString(jidBuf.toString());
-            jidBuf=null;
-        }
-        if (item==tJid) {
-            String s1=tJid.getString();
-            int at=tJid.getString().indexOf('@');
-            try {
-                updateChoice(s1.substring(at+1), tTranspList);
-            } catch (Exception e) {}
+    public void cmdOk() {
+        String jid=tJid.getValue();
+        if (jid!=null) {
+            boolean ask=tAskSubscrCheckBox.getValue();
+            sd.roster.storeContact(jid, tNick.getValue(), tGroup.getValue(), ask);
+            destroyView();
         }
     }
-    
+ 
     public void destroyView(){
         if (display!=null)   display.setCurrent(parentView/*roster*/);
     }
