b2be92d8a7fcdfe0394babc8e8365d493664b761
Vitaly Takmazov <vitalyster@gmail.com>
2012-01-19 10:01:23 +0000

    remove IME_ACTION_DONE

--- android/src/org/microemu/android/device/ui/AndroidTextBoxUI.java
+++ android/src/org/microemu/android/device/ui/AndroidTextBoxUI.java
@@ -62,16 +62,6 @@ public class AndroidTextBoxUI extends AndroidDisplayableUI implements TextBoxUI 
                     @Override
                     public InputConnection onCreateInputConnection(EditorInfo outAttrs) {
                         InputConnection connection = super.onCreateInputConnection(outAttrs);
-                        int imeActions = outAttrs.imeOptions & EditorInfo.IME_MASK_ACTION;
-                        if ((imeActions & EditorInfo.IME_ACTION_DONE) != 0) {
-                            // clear the existing action
-                            outAttrs.imeOptions ^= imeActions;
-                            // set the DONE action
-                            outAttrs.imeOptions |= EditorInfo.IME_ACTION_DONE;
-                        }
-                        if ((outAttrs.imeOptions & EditorInfo.IME_FLAG_NO_ENTER_ACTION) != 0) {
-                            outAttrs.imeOptions &= ~EditorInfo.IME_FLAG_NO_ENTER_ACTION;
-                        }
                         showNotify();
                         return connection;
                     }
