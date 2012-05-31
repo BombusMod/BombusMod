bb786f076f5f52b5f09a4cf7ffea3194e0656a32
Vitaly Takmazov <vitalyster@gmail.com>
2012-05-31 17:56:26 +0000

    + Android: unregister receivers at exit

--- android/src/org/bombusmod/BombusModService.java
+++ android/src/org/bombusmod/BombusModService.java
@@ -1,6 +1,5 @@
 package org.bombusmod;
 
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -8,24 +7,24 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.os.IBinder;
 import android.util.Log;
 import org.bombusmod.scrobbler.Receiver;
 
 public class BombusModService extends Service {
+
     public static final String ACTION_FOREGROUND = "FOREGROUND";
-
     private static final String LOG_TAG = "BombusModService";
-
-    private static final Class<?>[] mSetForegroundSignature = new Class[] {
+    private static final Class<?>[] mSetForegroundSignature = new Class[]{
         boolean.class};
-    private static final Class<?>[] mStartForegroundSignature = new Class[] {
+    private static final Class<?>[] mStartForegroundSignature = new Class[]{
         int.class, Notification.class};
-    private static final Class<?>[] mStopForegroundSignature = new Class[] {
+    private static final Class<?>[] mStopForegroundSignature = new Class[]{
         boolean.class};
-
     private NotificationManager mNM;
     private Method mSetForeground;
     private Method mStartForeground;
@@ -33,50 +32,51 @@ public class BombusModService extends Service {
     private Object[] mSetForegroundArgs = new Object[1];
     private Object[] mStartForegroundArgs = new Object[2];
     private Object[] mStopForegroundArgs = new Object[1];
+    
+    protected Receiver musicReceiver;
 
-void invokeMethod(Method method, Object[] args) {
-    try {
-        method.invoke(this, args);
-    } catch (InvocationTargetException e) {
-        // Should not happen.
-        Log.w(LOG_TAG, "Unable to invoke method", e);
-    } catch (IllegalAccessException e) {
-        // Should not happen.
-        Log.w(LOG_TAG, "Unable to invoke method", e);
+    void invokeMethod(Method method, Object[] args) {
+        try {
+            method.invoke(this, args);
+        } catch (InvocationTargetException e) {
+            // Should not happen.
+            Log.w(LOG_TAG, "Unable to invoke method", e);
+        } catch (IllegalAccessException e) {
+            // Should not happen.
+            Log.w(LOG_TAG, "Unable to invoke method", e);
+        }
     }
-}
-
 
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.i(LOG_TAG,"onStart();");
+        Log.i(LOG_TAG, "onStart();");
 
         mNM = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         try {
-        mStartForeground = getClass().getMethod("startForeground",
-                mStartForegroundSignature);
-        mStopForeground = getClass().getMethod("stopForeground",
-                mStopForegroundSignature);
-    } catch (NoSuchMethodException e) {
-        // Running on an older platform.
-        mStartForeground = mStopForeground = null;
-    }
-    try {
-        mSetForeground = getClass().getMethod("setForeground",
-                mSetForegroundSignature);
-    } catch (NoSuchMethodException e) {
-        throw new IllegalStateException(
-                "OS doesn't have Service.startForeground OR Service.setForeground!");
-    }
-        
+            mStartForeground = getClass().getMethod("startForeground",
+                    mStartForegroundSignature);
+            mStopForeground = getClass().getMethod("stopForeground",
+                    mStopForegroundSignature);
+        } catch (NoSuchMethodException e) {
+            // Running on an older platform.
+            mStartForeground = mStopForeground = null;
+        }
+        try {
+            mSetForeground = getClass().getMethod("setForeground",
+                    mSetForegroundSignature);
+        } catch (NoSuchMethodException e) {
+            throw new IllegalStateException(
+                    "OS doesn't have Service.startForeground OR Service.setForeground!");
+        }
+
         //Foreground Service
         Notification notification = new Notification(R.drawable.app_icon, getText(R.string.app_name), 0);
         PendingIntent contentIntent = PendingIntent.getActivity(this, 0, new Intent(this, BombusModActivity.class), 0);
         notification.setLatestEventInfo(this, getText(R.string.app_name), "", contentIntent);
         startForegroundCompat(R.string.app_name, notification);
-        
-      //audio scrobbler
+
+        //audio scrobbler
         IntentFilter filter = new IntentFilter();
         //Google Android player
         filter.addAction("com.android.music.playstatechanged");
@@ -120,7 +120,7 @@ void invokeMethod(Method method, Object[] args) {
         //PowerAmp
         filter.addAction("com.maxmpz.audioplayer.playstatechanged");
         //will be added any....
-      //scrobblers detect for players (poweramp for example)
+        //scrobblers detect for players (poweramp for example)
         //Last.fm
         filter.addAction("fm.last.android.metachanged");
         filter.addAction("fm.last.android.playbackpaused");
@@ -129,59 +129,70 @@ void invokeMethod(Method method, Object[] args) {
         filter.addAction("com.adam.aslfms.notify.playstatechanged");
         //Scrobble Droid
         filter.addAction("net.jjc1138.android.scrobbler.action.MUSIC_STATUS");
-        Receiver receiver = new Receiver(this);
-        this.registerReceiver(receiver, filter);
-      //scrobbling finished
+        musicReceiver = new Receiver(this);
+        registerReceiver(musicReceiver, filter);        
+
+        // enable network state receiver
+        ComponentName networkStateReceiver = new ComponentName(this, NetworkStateReceiver.class);
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(networkStateReceiver,
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
     }
 
     @Override
-        public void onDestroy() {
-            Log.i(LOG_TAG, "onDestroy();");
-            stopForegroundCompat(R.string.app_name);
+    public void onDestroy() {
+        Log.i(LOG_TAG, "onDestroy();");
+        stopForegroundCompat(R.string.app_name);
+        // disable network state receiver
+        ComponentName networkStateReceiver = new ComponentName(this, NetworkStateReceiver.class);
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(networkStateReceiver,
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        // unregister music receiver
+        unregisterReceiver(musicReceiver);
+    }
+
+    /**
+     * This is a wrapper around the new startForeground method, using the older
+     * APIs if it is not available.
+     */
+    void startForegroundCompat(int id, Notification notification) {
+        // If we have the new startForeground API, then use it.
+        if (mStartForeground != null) {
+            mStartForegroundArgs[0] = Integer.valueOf(id);
+            mStartForegroundArgs[1] = notification;
+            invokeMethod(mStartForeground, mStartForegroundArgs);
+            return;
         }
 
-/**
- * This is a wrapper around the new startForeground method, using the older
- * APIs if it is not available.
- */
-void startForegroundCompat(int id, Notification notification) {
-    // If we have the new startForeground API, then use it.
-    if (mStartForeground != null) {
-        mStartForegroundArgs[0] = Integer.valueOf(id);
-        mStartForegroundArgs[1] = notification;
-        invokeMethod(mStartForeground, mStartForegroundArgs);
-        return;
+        // Fall back on the old API.
+        mSetForegroundArgs[0] = Boolean.TRUE;
+        invokeMethod(mSetForeground, mSetForegroundArgs);
+        mNM.notify(id, notification);
     }
 
-    // Fall back on the old API.
-    mSetForegroundArgs[0] = Boolean.TRUE;
-    invokeMethod(mSetForeground, mSetForegroundArgs);
-    mNM.notify(id, notification);
-}
+    /**
+     * This is a wrapper around the new stopForeground method, using the older
+     * APIs if it is not available.
+     */
+    void stopForegroundCompat(int id) {
+        // If we have the new stopForeground API, then use it.
+        if (mStopForeground != null) {
+            mStopForegroundArgs[0] = Boolean.TRUE;
+            invokeMethod(mStopForeground, mStopForegroundArgs);
+            return;
+        }
 
-/**
- * This is a wrapper around the new stopForeground method, using the older
- * APIs if it is not available.
- */
-void stopForegroundCompat(int id) {
-    // If we have the new stopForeground API, then use it.
-    if (mStopForeground != null) {
-        mStopForegroundArgs[0] = Boolean.TRUE;
-        invokeMethod(mStopForeground, mStopForegroundArgs);
-        return;
+        // Fall back on the old API.  Note to cancel BEFORE changing the
+        // foreground state, since we could be killed at that point.
+        mNM.cancel(id);
+        mSetForegroundArgs[0] = Boolean.FALSE;
+        invokeMethod(mSetForeground, mSetForegroundArgs);
     }
-
-    // Fall back on the old API.  Note to cancel BEFORE changing the
-    // foreground state, since we could be killed at that point.
-    mNM.cancel(id);
-    mSetForegroundArgs[0] = Boolean.FALSE;
-    invokeMethod(mSetForeground, mSetForegroundArgs);
-}
 
     @Override
     public IBinder onBind(Intent arg0) {
         // TODO Auto-generated method stub
         return null;
     }
-
 }
