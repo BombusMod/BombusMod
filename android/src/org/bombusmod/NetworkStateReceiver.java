95c3f0811e9b37a774d8b5a35506a8fd879122f9
Vitaly Takmazov <vitalyster@gmail.com>
2012-10-30 08:54:50 +0000

    * Android: new network state receiver flow

--- android/src/org/bombusmod/NetworkStateReceiver.java
+++ android/src/org/bombusmod/NetworkStateReceiver.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.util.Log;
 
 import Client.StaticData;
 
@@ -18,21 +19,29 @@ import Client.StaticData;
  */
 public class NetworkStateReceiver extends BroadcastReceiver {
 
+    private boolean disconnected = false;
+
     @Override
     public void onReceive(Context context, Intent networkIntent) {
-        if (BombusModActivity.getInstance() == null)
+        if (BombusModActivity.getInstance() == null) {
             return;
-        if (StaticData.getInstance().roster == null)
-            return;        
-        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
-        if (activeNetwork == null) {
-            StaticData.getInstance().roster.errorLog("Connectivity lost");
-            StaticData.getInstance().roster.doReconnect();
-        } else {
-            if (activeNetwork.isConnectedOrConnecting() && StaticData.getInstance().roster.isLoggedIn()) {
-                StaticData.getInstance().roster.errorLog(activeNetwork.getTypeName() + " connected");
-            }
         }
+        if (StaticData.getInstance().roster == null) {
+            return;
+        }
+        if (networkIntent.getBooleanExtra(ConnectivityManager.EXTRA_NO_CONNECTIVITY, false)) {
+            if (!disconnected) {
+                StaticData.getInstance().roster.errorLog("Connectivity lost");
+                Log.d(NetworkStateReceiver.class.getSimpleName(), "Connectivity lost");
+                disconnected = true;
+                StaticData.getInstance().roster.logoff(null);
+            }
+        } else {
+            if (disconnected) {
+                disconnected = false;
+                Log.d(NetworkStateReceiver.class.getSimpleName(), "Connected");
+                StaticData.getInstance().roster.doReconnect();
+            }
+        }       
     }
 }
