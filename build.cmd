993a7c335290b0c1260cd851a02f2a68f7324c03
Vitaly Takmazov <vitalyster@gmail.com>
2012-11-16 02:00:58 +0000

    Update Netbeans project to latest versions (JDK7, Netbeans 7.2, JavaME SDK 3.2), fix abilities

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 @echo off
-set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_26\
-set ANT_EXEC="C:\Program Files\NetBeans 7.1.1\java\ant\bin\ant.bat"
+set JAVA_HOME=C:\Program Files\Java\jdk1.7.0_07\
+set ANT_EXEC="C:\Program Files\NetBeans 7.2\java\ant\bin\ant.bat"
 call %ANT_EXEC% %*
