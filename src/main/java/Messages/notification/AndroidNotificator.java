665dfccc977dfcdf9e5a181f2cd9c39bc47ed7aa
Vitaly Takmazov <vitalyster@gmail.com>
2018-11-24 01:24:17 +0300

    Add notification channel

--- src/main/java/Messages/notification/AndroidNotificator.java
+++ src/main/java/Messages/notification/AndroidNotificator.java
@@ -2,11 +2,13 @@
 package Messages.notification;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import org.bombusmod.BombusModActivity;
 import org.bombusmod.R;
@@ -18,12 +20,31 @@ public class AndroidNotificator implements Notificator {
 
     public static final int NOTIFY_ID = 1;
 
-    public NotificationManager getNotificationManager(){
-        return (NotificationManager) BombusModActivity.getInstance().getSystemService(Context.NOTIFICATION_SERVICE);
+    private static final String CHANNEL_ID = "default";
+
+    private NotificationManager notificationManager;
+
+    public AndroidNotificator() {
+        notificationManager = (NotificationManager) BombusModActivity.getInstance().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT < 26) {
+            return;
+        }
+
+        NotificationChannel channel =  notificationManager.getNotificationChannel(CHANNEL_ID);
+        if (channel == null) {
+            channel = new NotificationChannel(CHANNEL_ID,
+                    "BombusMod",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription("New messages");
+            channel.enableLights(true);
+            channel.enableVibration(true);
+            channel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     public void sendNotify(final String title, final String text) {
-        NotificationManager mNotificationManager = (NotificationManager) BombusModActivity.getInstance().getSystemService(Context.NOTIFICATION_SERVICE);
         long when = 0;
         int icon = R.drawable.inc_msg;
         long[] vibraPattern = {0, 500, 250, 500};
@@ -32,9 +53,10 @@ public class AndroidNotificator implements Notificator {
         } else {
             Context context = BombusModActivity.getInstance();
             
-            NotificationCompat.Builder notification = new NotificationCompat.Builder(context);
-            CharSequence contentTitle = (String) context.getApplicationContext().getText(R.string.notifyTitle);
-            CharSequence contentText = (String) context.getApplicationContext().getText(R.string.notifyInfo) + ": " + StaticData.getInstance().roster.highliteMessageCount;
+            NotificationCompat.Builder notification = Build.VERSION.SDK_INT < 26 ?
+                    new NotificationCompat.Builder(context) : new NotificationCompat.Builder(context, CHANNEL_ID);
+            CharSequence contentTitle = context.getApplicationContext().getText(R.string.notifyTitle);
+            CharSequence contentText = context.getApplicationContext().getText(R.string.notifyInfo) + ": " + StaticData.getInstance().roster.highliteMessageCount;
             Intent notificationIntent = new Intent(context, BombusModActivity.class);
             notificationIntent.setAction("org.bombusmod.bm-notify");
             PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, PendingIntent.FLAG_ONE_SHOT);
@@ -42,7 +64,7 @@ public class AndroidNotificator implements Notificator {
             replyIntent.setAction("org.bombusmod.bm-notify.reply");
             PendingIntent piReply = PendingIntent.getActivity(context, 0, replyIntent, PendingIntent.FLAG_ONE_SHOT);
             //notification.setLights(0xff00ff00, 300, 1000);
-            //notification.setVibrate(vibraPattern);
+            notification.setVibrate(vibraPattern);
             notification.setDefaults(Notification.DEFAULT_ALL);
             notification.setContentIntent(contentIntent);
             notification.setContentTitle(contentTitle);
@@ -56,12 +78,11 @@ public class AndroidNotificator implements Notificator {
             
             notification.setStyle(style);
                     
-            mNotificationManager.notify(NOTIFY_ID, notification.build());
+            notificationManager.notify(NOTIFY_ID, notification.build());
         }
     }
     public void clear() {
-        NotificationManager mNotificationManager = (NotificationManager) BombusModActivity.getInstance().getSystemService(Context.NOTIFICATION_SERVICE);
-        mNotificationManager.cancel(NOTIFY_ID);
+        notificationManager.cancel(NOTIFY_ID);
      }
 }
 //#endif
